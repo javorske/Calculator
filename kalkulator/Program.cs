75b9e09f1c471c2099d2a6fec535353bd1892b08
javorske <jaywors@gmail.com>
2019-10-28 11:59:42 +0100

    Dodanie opcji dodawania liczb

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -10,6 +10,29 @@ namespace kalkulator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wybierz opcje:");
+            Console.WriteLine("1. Dodawanie.");
+            Console.WriteLine("2. Odejmowanie.");
+            Console.WriteLine("3. Mnozenie.");
+            Console.WriteLine("4. Dzielenie.");
+            int check = int.Parse(Console.ReadLine());
+
+            switch (check)
+            {
+                case 1:
+                    Console.WriteLine(Dodawanie());
+                    Console.ReadKey();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public static int Dodawanie()
+        { 
+            Console.WriteLine("Podaj pierwsza liczbe");
+            int liczba1 = int.Parse(Console.ReadLine());
+            int liczba2 = int.Parse(Console.ReadLine());
+            return liczba1 + liczba2;
         }
     }
 }
