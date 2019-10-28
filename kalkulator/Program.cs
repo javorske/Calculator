eac100a3547075ae912abb3bddaf03b2fd29b95c
javorske <jaywors@gmail.com>
2019-10-28 13:00:16 +0100

    Fixes #6 Added multiplication with DivideByZeroException handling

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -10,27 +10,49 @@ namespace kalkulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wybierz opcje:");
-            Console.WriteLine("1. Dodawanie.");
-            Console.WriteLine("2. Odejmowanie.");
-            Console.WriteLine("3. Mnozenie.");
-            Console.WriteLine("4. Dzielenie.");
-            int check = int.Parse(Console.ReadLine());
-
-            switch (check)
+            bool exitLoop = true;
+            int check = 0;
+            do
             {
-                case 1:
-                    Dodawanie();
-                    break;
-                case 2:
-                    Odejmowanie();
-                    break;
-                case 3:
-                    Mnozenie();
-                    break;
-                default:
-                    break;
-            }
+                try
+                {
+                
+                    Console.WriteLine("Wybierz opcje:");
+                    Console.WriteLine("1. Dodawanie.");
+                    Console.WriteLine("2. Odejmowanie.");
+                    Console.WriteLine("3. Mnozenie.");
+                    Console.WriteLine("4. Dzielenie.");
+                    check = int.Parse(Console.ReadLine());
+                    switch (check)
+                    {
+                        case 1:
+                            Dodawanie();
+                            break;
+                        case 2:
+                            Odejmowanie();
+                            break;
+                        case 3:
+                            Mnozenie();
+                            break;
+                        case 4:
+                            Dzielenie();
+                            break;
+                        default:
+                            SwitchDefault();
+                            break;
+                        case 0:
+                            exitLoop = false;
+                            break;
+                    }
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Nie wybrano zadnej opcji");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            } while (exitLoop);
         }
         public static void Dodawanie()
         { 
@@ -40,6 +62,7 @@ namespace kalkulator
             int liczba2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Wynik dodawania to: " + (liczba1 + liczba2) + ".") ;
             Console.ReadKey();
+            Console.Clear();
         }
         public static void Odejmowanie()
         {
@@ -49,6 +72,7 @@ namespace kalkulator
             int liczba2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Wynik odejmowania to: " + (liczba1 - liczba2) + ".");
             Console.ReadKey();
+            Console.Clear();
         }
         public static void Mnozenie()
         {
@@ -58,6 +82,30 @@ namespace kalkulator
             int liczba2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Wynik mnozenia to: " + (liczba1 * liczba2) + ".");
             Console.ReadKey();
+            Console.Clear();
+        }
+        public static void Dzielenie()
+        {
+            Console.WriteLine("Podaj pierwsza liczbe: ");
+            int liczba1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe: ");
+            int liczba2 = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Wynik dzielenia to: " + (liczba1 / liczba2) + ".");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Nie mozna dzielic przez zero.");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void SwitchDefault()
+        {
+            Console.WriteLine("Nie wybrano opcji.");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
