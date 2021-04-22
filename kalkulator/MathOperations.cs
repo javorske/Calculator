a9d2090342c2569784913ce9732a2a1f790141e9
javorske <jaywors@gmail.com>
2021-04-22 16:55:34 +0200

    Operations methods moved to new Class MathOperationsClass

---
+++ kalkulator/MathOperations.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulator
+{
+    public class MathOperations
+    {
+        public void Sum()
+        {
+            Console.WriteLine("Podaj pierwsza liczbe: ");
+            int liczba1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe: ");
+            int liczba2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wynik dodawania to: " + (liczba1 + liczba2) + ".");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void Subtraction()
+        {
+            Console.WriteLine("Podaj pierwsza liczbe: ");
+            int liczba1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe: ");
+            int liczba2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wynik odejmowania to: " + (liczba1 - liczba2) + ".");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void Multiplication()
+        {
+            Console.WriteLine("Podaj pierwsza liczbe: ");
+            int liczba1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe: ");
+            int liczba2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Wynik mnozenia to: " + (liczba1 * liczba2) + ".");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void Divide()
+        {
+            Console.WriteLine("Podaj pierwsza liczbe: ");
+            int liczba1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe: ");
+            int liczba2 = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Wynik dzielenia to: " + (liczba1 / liczba2) + ".");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Nie mozna dzielic przez zero.");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -11,31 +11,32 @@ namespace kalkulator
         static void Main(string[] args)
         {
             bool exitLoop = true;
-            int check;
+            int caseChoice;
+            MathOperations mathOperations = new MathOperations();
             do
             {
                 try
-                {
-                
+                {                
                     Console.WriteLine("Wybierz opcje:");
                     Console.WriteLine("1. Dodawanie.");
                     Console.WriteLine("2. Odejmowanie.");
                     Console.WriteLine("3. Mnozenie.");
                     Console.WriteLine("4. Dzielenie.");
-                    check = int.Parse(Console.ReadLine());
-                    switch (check)
+                    Console.WriteLine("0. Zakończ.");
+                    caseChoice = int.Parse(Console.ReadLine());
+                    switch (caseChoice)
                     {
                         case 1:
-                            Dodawanie();
+                            mathOperations.Sum();
                             break;
                         case 2:
-                            Odejmowanie();
+                            mathOperations.Subtraction();
                             break;
                         case 3:
-                            Mnozenie();
+                            mathOperations.Multiplication();
                             break;
                         case 4:
-                            Dzielenie();
+                            mathOperations.Divide();
                             break;
                         default:
                             SwitchDefault();
@@ -54,53 +55,7 @@ namespace kalkulator
 
             } while (exitLoop);
         }
-        public static void Dodawanie()
-        { 
-            Console.WriteLine("Podaj pierwsza liczbe: ");
-            int liczba1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj druga liczbe: ");
-            int liczba2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Wynik dodawania to: " + (liczba1 + liczba2) + ".") ;
-            Console.ReadKey();
-            Console.Clear();
-        }
-        public static void Odejmowanie()
-        {
-            Console.WriteLine("Podaj pierwsza liczbe: ");
-            int liczba1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj druga liczbe: ");
-            int liczba2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Wynik odejmowania to: " + (liczba1 - liczba2) + ".");
-            Console.ReadKey();
-            Console.Clear();
-        }
-        public static void Mnozenie()
-        {
-            Console.WriteLine("Podaj pierwsza liczbe: ");
-            int liczba1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj druga liczbe: ");
-            int liczba2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Wynik mnozenia to: " + (liczba1 * liczba2) + ".");
-            Console.ReadKey();
-            Console.Clear();
-        }
-        public static void Dzielenie()
-        {
-            Console.WriteLine("Podaj pierwsza liczbe: ");
-            int liczba1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj druga liczbe: ");
-            int liczba2 = int.Parse(Console.ReadLine());
-            try
-            {
-                Console.WriteLine("Wynik dzielenia to: " + (liczba1 / liczba2) + ".");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Nie mozna dzielic przez zero.");
-            }
-            Console.ReadKey();
-            Console.Clear();
-        }
+        
         public static void SwitchDefault()
         {
             Console.WriteLine("Nie wybrano opcji.");
