21d367613136443205e14182fb98a745629febbf
javorske <jaywors@gmail.com>
2021-04-24 12:52:48 +0200

    Fixes #9 Changed all folders and files names from kalkulator to Calculator

---
+++ Calculator/NumbersGetter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    abstract class NumbersGetter : IMathOperations
+    {
+        public OperationNumbers EnterNumbers()
+        {
+            Console.Clear();
+            OperationNumbers numbers = new OperationNumbers();
+            Console.WriteLine("Enter first number: ");
+            numbers.Number1 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            numbers.Number2 = decimal.Parse(Console.ReadLine());
+            return numbers;
+        }
+        public void ClearConsole()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public abstract void MathOperation();
+    }
+}
